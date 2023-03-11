0e618d9bbaa784838b744db30536b140d99bf5d5
Yul Kang <39001942+kangyul@users.noreply.github.com>
2023-03-12 00:15:04 +0900

    Time: 3 ms (84.13%), Space: 6.4 MB (95.97%) - LeetHub

---
+++ 0299-bulls-and-cows/0299-bulls-and-cows.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        int bulls = 0, cows = 0;
+        vector<int> sVec(10, 0);
+        vector<int> gVec(10, 0);
+
+        for (int i = 0; i < secret.size(); i++) {
+            if(secret[i] == guess[i]) {
+                bulls++;
+            } else {
+                sVec[secret[i] - '0']++;
+                gVec[guess[i] - '0']++;
+            }
+        }
+        
+        for (int i = 0; i < 10; i++) {
+            cows += min(sVec[i], gVec[i]);
+        }
+
+        return to_string(bulls) + 'A' + to_string(cows) + 'B';
+    }
+};
